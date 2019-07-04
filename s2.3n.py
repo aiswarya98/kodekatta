539766314570ddd5722c5b26dd5d9803f9158558
Avaneeshj <52447478+Avaneeshj@users.noreply.github.com>
2019-07-04 10:42:44 +0530

    Update s2.3n.py

--- s2.3n.py
+++ s2.3n.py
@@ -1,10 +1,7 @@
-a=int(input())
-if a>1:
-    for i in range(2,a//2):
-        if(a%i==0):
-            print("no")
-            break
-    else:
-        print("yes")
-else:
+no=int(input())
+for i in range(2,no):
+  if(no%i==0):
     print("no")
+    break
+else:
+  print("yes")
